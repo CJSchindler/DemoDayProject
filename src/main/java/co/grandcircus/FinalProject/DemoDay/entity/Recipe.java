55a11203d31a4dba62b06214ae574ab1d41daffd
Amna Anjum <amnaanjum@Amnas-MacBook-Air.local>
2018-09-07 14:33:16 -0400

    added to list

--- src/main/java/co/grandcircus/FinalProject/DemoDay/entity/Recipe.java
+++ src/main/java/co/grandcircus/FinalProject/DemoDay/entity/Recipe.java
@@ -1,16 +1,28 @@
 package co.grandcircus.FinalProject.DemoDay.entity;
 
+import java.util.List;
+
 public class Recipe {
 
 	private String label;
+	private String image;
+	private String url;
+	private int yield;
+	List <String> ingredientLines;
+	private int totalTime;
 
 	public Recipe() {
 		super();
 	}
 
-	public Recipe(String label) {
+	public Recipe(String label, String image, String url, int yield, List<String> ingredientLines, int totalTime) {
 		super();
 		this.label = label;
+		this.image = image;
+		this.url = url;
+		this.yield = yield;
+		this.ingredientLines = ingredientLines;
+		this.totalTime = totalTime;
 	}
 
 	public String getLabel() {
@@ -20,6 +32,47 @@ public class Recipe {
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getyield() {
+		return yield;
+	}
+
+	public void setyield(int yield) {
+		this.yield = yield;
+	}
+
+	public List<String> getIngredientLines() {
+		return ingredientLines;
+	}
+
+	public void setIngredientLines(List<String> ingredientLines) {
+		this.ingredientLines = ingredientLines;
+	}
+
+	public int getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(int totalTime) {
+		this.totalTime = totalTime;
+	}
+
 	
 	
 }
--- src/main/webapp/WEB-INF/views/display.jsp
+++ src/main/webapp/WEB-INF/views/display.jsp
@@ -12,7 +12,13 @@
 <c:forEach var="recipe" items="${ recipelist }">
 		<div>
 			<h5>${recipe.recipe.label}</h5>
-			
+			<h5><img src="${recipe.recipe.image}"></h5>
+			<h5><a href="${recipe.recipe.url}">Go to Recipe </a></h5>
+			<h5>Serving size: ${recipe.recipe.yield}</h5>
+			Recipe: <c:forEach var="ingredient" items="${recipe.recipe.ingredientLines}"> 
+			<h5>${ingredient} </h5>
+			</c:forEach>
+			<h5>Total Time: ${recipe.recipe.totalTime}</h5>
 			
 		</div>
 	</c:forEach>
