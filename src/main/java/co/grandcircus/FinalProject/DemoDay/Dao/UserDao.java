0b0f91b23bc56536294991ebe8173d087e3a7dec
shontinique_uqdah <shontini@umich.edu>
2018-09-10 10:14:27 -0400

    Added calendar, login, sessions, logout, and updated UserDao

--- src/main/java/co/grandcircus/FinalProject/DemoDay/Dao/UserDao.java
+++ src/main/java/co/grandcircus/FinalProject/DemoDay/Dao/UserDao.java
@@ -8,13 +8,64 @@ import org.springframework.stereotype.Repository;
 
 import co.grandcircus.FinalProject.DemoDay.entity.User;
 
+
+import java.util.List;
+
+
+
+
+
+
 @Repository
 @Transactional
 public class UserDao {
+	
 	@PersistenceContext
-	EntityManager em;
+	private EntityManager em;
+	
+	public List<User> findAll() {
+				return em.createQuery("FROM User", User.class).getResultList();
+		
+	}
+	
+	public User findByID(Long id) {
+		return em.find(User.class, id);
+	}
+	
+	
+	
+	//This works, but the below way is better
+//	public User findByEmail(String email) {
+//		return em.createQuery("FROM User WHERE email LIKE :email", User.class)
+//				.setParameter("email", "%" + email + "%")
+//				.getSingleResult();
+//	}
+	
+	public User findByEmail(String email) {
+		return em.createQuery("FROM User WHERE email = :email", User.class)
+				.setParameter("email", email)
+				.getSingleResult();
+	}
+	
 	
 	public void create(User user) {
-		em.merge(user);
+		em.persist(user);
 	}
+	
+	public void delete(Long id) {
+		User user = em.getReference(User.class, id);
+		em.remove(user);
+	}
+	
+	public void update(User user) {
+		em.merge(user);
+		
+	}
+
+	
+
+
+	
+	
+
 }
--- src/main/java/co/grandcircus/FinalProject/DemoDay/RecipeController.java
+++ src/main/java/co/grandcircus/FinalProject/DemoDay/RecipeController.java
@@ -4,6 +4,8 @@ package co.grandcircus.FinalProject.DemoDay;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import co.grandcircus.FinalProject.DemoDay.dao.UserDao;
 //import co.grandcircus.FinalProject.DemoDay.dao.MenuItemDao;
@@ -78,6 +81,12 @@ public class RecipeController {
 
 		}
 	
+	@RequestMapping("/calendar")
+	public ModelAndView showCalendar() {
+		ModelAndView mav = new ModelAndView("calendar");
+		return mav;
+	}
+	
 	@RequestMapping("/display")
 	public ModelAndView showSearch() {
 		ModelAndView mav = new ModelAndView("display");
@@ -130,5 +139,43 @@ public class RecipeController {
 		ModelAndView mav = new ModelAndView("display");
 //		mav.addObject("menuItem", menuItem);
 		return mav;
+	}
+	
+	@RequestMapping("/login")
+	public ModelAndView showLoginForm() {
+		return new ModelAndView("login-form");
+	}
+
+	@PostMapping("/login")
+	// get the username and password from the form when it's submitted.
+	public ModelAndView submitLoginForm(@RequestParam("email") String email, @RequestParam("password") String password,
+			HttpSession session, RedirectAttributes redir) {
+		// Find the matching user.
+		User user = userDao.findByEmail(email);
+		if (user == null || !password.equals(user.getPassword())) {
+			// If the user or password don't match, display an error message.
+			ModelAndView mav = new ModelAndView("login");
+			mav.addObject("message", "Incorrect username or password.");
+			return mav;
+		}
+		
+		// On successful login, add the user to the session.
+		session.setAttribute("user", user);
+		
+		// A flash message will only show on the very next page. Then it will go away.
+		// It is useful with redirects since you can't add attributes to the mav.
+		redir.addFlashAttribute("message", "Logged in.");
+		return new ModelAndView("redirect:/display");
+	}
+
+	@RequestMapping("/logout")
+	public ModelAndView logout(HttpSession session, RedirectAttributes redir) {
+		// invalidate clears the current user session and starts a new one.
+		session.invalidate();
+		
+		// A flash message will only show on the very next page. Then it will go away.
+		// It is useful with redirects since you can't add attributes to the mav.
+		redir.addFlashAttribute("message", "Logged out.");
+		return new ModelAndView("redirect:/");
 	}
 }
